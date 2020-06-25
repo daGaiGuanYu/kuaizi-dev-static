4b9600a3ab5f2e4368f645268d6fa07113678a51
关于 <372301467@qq.com>
2020-06-25 09:48:16 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@
 const Url = require('url')
 const Path = require('path')
 const FS = require('fs')
-const Http = require('http')
 
 /**
- * @param {Http.IncomingMessage} req 
- * @param {Http.ServerResponse} res 
+ * @param {import('http').IncomingMessage} req 
+ * @param {import('http').ServerResponse} res 
  * @param {String} dir 
+ * @returns {import('stream').Writable|boolean}
  */
 module.exports = function(req, res, dir){
   const filename = Url.parse(req.url).query
   const path = Path.join(dir, filename)
   if(!FS.existsSync(path))
-    return true // 真代表错误
-  FS.createReadStream(path).pipe(res)
+    return true // 真，代表错误
+  return FS.createReadStream(path).pipe(res)
 }
